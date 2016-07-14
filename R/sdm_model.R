7013a43d4f50c78da4bdec8536322ce751abbdd0
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-07-14 14:42:00 -0700

    code edits

--- R/sdm_model.R
+++ R/sdm_model.R
@@ -1,15 +1,14 @@
-library("ncf")
-library("xtable")
-library("gbm")
-library("doMC")
-library("dismo")
+require("ncf")
+require("xtable")
+require("gbm")
+require("doMC")
+require("dismo")
+require("ggplot2")
 
 species.table <- read.delim("data/species_list.csv", header=T, sep=",")
 
-#construct metadata for all species
-for(i in 1:nrow(species.table)) {
-assign(paste(species.table[i,3],".data",sep=""),read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=","))
-}
+sdm.colors = colorRampPalette(c("white","red"))
+sdm.bw = colorRampPalette(c("white","black"))
 
 #fit BRT models for all species
 registerDoMC(detectCores() - 1)
@@ -54,7 +53,7 @@ brt.models.simp <- foreach(i = 1:nrow(species.table), .packages = c("gbm","dismo
   model <- gbm.step(data = data, gbm.x = predlists[[i]], gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5)
   model
 }
-save(brt.models.simp, file = "data/brt_models")
+save(brt.models.simp, file = "data/brt_models_simp")
 #load(file = "data/brt_models")
 
 
@@ -66,23 +65,23 @@ brt.models.output <- foreach(i = 1:nrow(species.table), .packages = c("gbm","dis
   model <- brt.models.simp[[i]]
   #model <- brt.models[[i]]
   model.preds <- predict(vars, model, n.trees=model[["gbm.call"]][["best.trees"]], type="response")
-  writeRaster(model.preds, filename=paste("data/Preds/",toupper(species.table[i,3]),".asc",sep=""), format="ascii", overwrite=TRUE)
+  writeRaster(model.preds, filename=paste("output/",toupper(species.table[i,2]),"_preds.asc",sep=""), format="ascii", overwrite=TRUE)
   plot(model.preds, col=sdm.colors(100), axes=F, box=FALSE)
   #summary(model)
 }
 
 for(i in 1:nrow(species.table)) {
-  assign(paste(toupper(species.table[i,3]),sep=""),raster(paste("/home/casey/Research/Projects/SDMs/Data/Preds/",toupper(species.table[i,3]),".asc",sep="")))
-  png(paste("/home/casey/Research/Graphics_Repo/",toupper(species.table[i,3]),"_SDM2.png",sep=""), bg = "white", width = 1000, height = 700, pointsize = 24)
+  assign(paste(toupper(species.table[i,2]),sep=""),raster(paste("output/",toupper(species.table[i,2]),".asc",sep="")))
+  png(paste("figs/",toupper(species.table[i,2]),"_preds.png",sep=""), bg = "transparent", width = 1000, height = 700, pointsize = 24)
   par(mar=c(0,0,0,0)+0.0)
-  plot(get(paste(toupper(species.table[i,3]))), col=sdm.colors(100), axes=F, box=FALSE)
+  plot(get(paste(toupper(species.table[i,2]))), col=sdm.colors(100), axes=F, box=FALSE)
   dev.off()
 }
 
 #summarize model output data
 brt_deviance <- data.frame("SPP"=rep(NA,nrow(species.table)),"DEV"=rep(NA,nrow(species.table)),"ERR"=rep(NA,nrow(species.table)),"ROC"=rep(NA,nrow(species.table)),"ROCERR"=rep(NA,nrow(species.table)))
 for(i in 1:nrow(species.table)) {
-  brt_deviance[i,"SPP"] <- toupper(paste(species.table[i,3]))
+  brt_deviance[i,"SPP"] <- toupper(paste(species.table[i,2]))
   brt_deviance[i,"DEV"] <- ((brt.models.simp[i][[1]][["self.statistics"]][["mean.null"]] - brt.models.simp[i][[1]][["cv.statistics"]][["deviance.mean"]])/brt.models.simp[i][[1]][["self.statistics"]][["mean.null"]])*100
   #brt_deviance[i,"DEV"] <- ((brt.models[i][[1]][["self.statistics"]][["mean.null"]] - brt.models[i][[1]][["cv.statistics"]][["deviance.mean"]])/brt.models[i][[1]][["self.statistics"]][["mean.null"]])*100
   brt_deviance[i,"ERR"] <- (brt.models.simp[i][[1]][["cv.statistics"]][["deviance.se"]]/brt.models.simp[i][[1]][["self.statistics"]][["mean.null"]])*100
@@ -92,7 +91,7 @@ for(i in 1:nrow(species.table)) {
   brt_deviance[i,"ROCERR"] <- brt.models.simp[i][[1]][["cv.statistics"]][["discrimination.se"]]
   #brt_deviance[i,"ROCERR"] <- brt.models[i][[1]][["cv.statistics"]][["discrimination.se"]]
 }
-write.csv(brt_deviance, file = "/home/casey/Research/Projects/SDMs/Data/brt_devs.csv", row.names=FALSE)
+write.csv(brt_deviance, file = "data/brt_devs.csv", row.names=FALSE)
 
 
 brt_sums <- data.frame(rep(NA,length(ascii.names)))
@@ -102,7 +101,7 @@ for(i in 1:nrow(species.table)) {
   x.var <- data.frame(brt.models.output[i],stringsAsFactors=FALSE)
   x.var[2] <- round(x.var[2],2)
   brt_sums <- merge(brt_sums,x.var,by.x="Predictor",by.y="var",all.x=TRUE)
-  colnames(brt_sums)[i+1] <- toupper(paste(species.table[i,3]))
+  colnames(brt_sums)[i+1] <- toupper(paste(species.table[i,2]))
   brt_sums[is.na(brt_sums)] <- 0
   #brt_sums[brt_sums==max(na.omit(brt_sums[i+1]))] <- paste("\\B{",max(na.omit(brt_sums[i+1])),"}",sep="")
 }
@@ -121,33 +120,31 @@ brt_sums_wt <- brt_sums
 brt_sums$Total <- NULL
 brt_sums$wTotal <- NULL
 
-
+#construct LaTex table 
 for(i in 2:ncol(brt_sums)) {
   sums <- as.numeric(brt_sums[,i])
   sums[which(sums == max(sums), arr.ind=TRUE)] <- paste("\\B{",max(sums),"}",sep="")
   brt_sums[,i] <- sums
 }
 
-#construct LaTex table 
 brt_sums[brt_sums==0] <- "---"
 print(xtable(brt_sums), include.rownames=FALSE, sanitize.text.function=function(x){x}, floating=FALSE)  
 
-write.csv(brt_sums, file = "/home/casey/Research/Projects/SDMs/Data/brt_sums.csv", row.names=FALSE)
+write.csv(brt_sums, file = "data/brt_sums.csv", row.names=FALSE)
 
 
 #calculate spatial autocorrelation across all species
-
 auto <- foreach(i = 1:nrow(species.table), .packages = c("ncf"), .combine="rbind") %do% {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  #model <- mget(paste(species.table[i,3],".brt",sep=""))[[paste(species.table[i,3],".brt",sep="")]]
+  data <- read.delim(paste("data/",species.table[i,2],".data",sep=""), header=T, sep=",")
+  #model <- mget(paste(species.table[i,2],".brt",sep=""))[[paste(species.table[i,2],".brt",sep="")]]
   model <- brt.models.simp[[i]]
   #model <- brt.models[[i]]
   cor <- correlog(data[,1], data[,2], resid(model), increment=1000, resamp=0, latlon=FALSE)
-  temp_df <- data.frame(x=as.numeric(names(cor$correlation[2:21])), y=cor$correlation[2:21], col=rep(toupper(species.table[i,3]), each=length(cor$correlation[2:21])))
+  temp_df <- data.frame(x=as.numeric(names(cor$correlation[2:21])), y=cor$correlation[2:21], col=rep(toupper(species.table[i,2]), each=length(cor$correlation[2:21])))
   temp_df
-}  
+} 
 
-shapes <- unlist(lapply(c("1", "2", "3", "4", "5", "6", "7"), utf8ToInt))
+shapes <- unlist(lapply(c("1", "2", "3", "4", "5", "6"), utf8ToInt))
 
 ggplot(auto,aes(x=x,y=y,group=col,shape=col)) + 
   geom_line(colour=c("grey70"),size=.75) + 
@@ -162,205 +159,3 @@ ggplot(auto,aes(x=x,y=y,group=col,shape=col)) +
   scale_shape_manual(values=shapes) + 
   geom_hline(aes(yintercept=0), linetype=2) + 
   scale_x_continuous(breaks=seq(1, 20, 1))
-
-
-#plot effect of greenness for all species
-green <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="GREEN",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  green <- rbind(green,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(green,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab("Seasonal Change in Vegetation Greenness") + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#plot effect of annual temperature range for all species
-tempanrange <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="TEMPANRANGE",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$x <- values$x/10
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  tempanrange <- rbind(tempanrange,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(tempanrange,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab(expression(paste("Annual Range of Temperature (",degree,"C)",sep=""))) + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) + 
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#plot effect of elevation for all species
-elev <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="ELEV",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  elev <- rbind(elev,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(elev,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab("Elevation (m above sea level)") + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#plot effect of precipitation of driest month for all species
-precdm <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="PRECDM",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  precdm <- rbind(precdm,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(precdm,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab("Precipitation of Driest Month (mm)") + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) + 
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#plot effect of light for all species
-light <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="LIGHT",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  light <- rbind(light,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(light,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab("Artificial Light (relative)") + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#plot effect of tree density for all species
-tree <- NULL
-for (i in 1:nrow(species.table)) {
-  data <- read.delim(paste("/home/casey/Research/Projects/SDMs/Data/",species.table[i,3],".data",sep=""), header=T, sep=",")
-  model <- brt.models.simp[[i]]
-  values <- plot.gbm(model, i.var="TREEDENS",return.grid=TRUE, type="response")
-  colnames(values) <- c("x","y")
-  values$col <- as.factor(rep(paste(toupper(species.table[i,3])), each=length(values[,2])))
-  tree <- rbind(tree,values)
-  rm(data)
-  rm(model)
-  rm(values)
-}  
-ggplot(tree,aes(x=x,y=y,group=col,colour=col)) + 
-  geom_line(size=1) + 
-  ylab("Occurence (Pr)") + 
-  xlab("Tree Density (percent coverage)") + 
-  labs(color = "Species") + 
-  theme_bw() + 
-  theme(legend.key = element_blank()) +
-  theme(text = element_text(size = 20)) +
-  scale_colour_manual(values=plotPal)
-
-
-#model based on residual autocorrelation
-rast_brt <- r
-
-xy <- cbind(egk.data$x, egk.data$y)  
-
-xy_res_brt <- cbind(xy, resid(brt.models[[1]]))
-
-rast_brt[cellFromXY(rast_brt, xy_res_brt)] <- xy_res_brt[,3]
-
-focal_rac_rast_brt <- focal(rast_brt, w=matrix(1,3,3), fun = mean, na.rm = TRUE)
-
-RAC <- extract(focal_rac_rast_brt, xy)
-
-egk.data.spatial <- cbind(egk.data,RAC)
-
-set.seed(123)
-egk.brt.spatial <- gbm.step(data = egk.data.spatial, gbm.x = c(predlists[[i]],22), gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5)
-
-
-library("spdep")
-
-nb.list <- dnearneigh(as.matrix(egk.data[,c("x", "y")]), 0, 3000)
-
-nb.weights <- nb2listw(nb.list, zero.policy=TRUE)
-
-nbd <- nbdists(nb.list, xy)
-gl <- lapply(nbd, function(x) 1/x)
-lw <- nb2listw(nb.list, glist=gl, zero.policy=TRUE)
-AC <- lag(lw, egk.data$occ)
-
-AC[is.na(AC)] <- 0
-
-egk.data.spatial <- cbind(egk.data.spatial,AC)
-
-
-rast_brt <- r
-
-xy <- cbind(egk.data$x, egk.data$y)  
-
-xy_res_brt <- cbind(xy, egk.data$occ)
-
-rast_brt[cellFromXY(rast_brt, xy_res_brt)] <- xy_res_brt[,3]
-
-focal_rac_rast_brt <- focal(rast_brt, w=matrix(1,3,3), fun = sum, na.rm = TRUE)
-
-AC <- extract(focal_rac_rast_brt, xy)
-
-egk.data.spatial <- cbind(egk.data.spatial,AC)
-
-set.seed(123)
-egk.brt.spatial3 <- gbm.step(data = egk.data.spatial, gbm.x = c(predlists[[i]],22), gbm.y = 3, family = "bernoulli", tree.complexity = 5, learning.rate = 0.005, bag.fraction = 0.5)
-
-
-
-egk.preds.spatial1 <- predict(stack(vars,"AC"=focal_rac_rast_brt), model, n.trees=model[["gbm.call"]][["best.trees"]], type="response")
