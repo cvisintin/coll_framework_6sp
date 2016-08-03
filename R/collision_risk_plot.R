5f34068ac7b3a877851f5ac84d568a850e4d6093
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-08-02 17:34:47 -0700

    added code to plot seasonal model effects

--- R/collision_risk_plot.R
+++ R/collision_risk_plot.R
@@ -132,3 +132,39 @@ shapes <- unlist(lapply(c("1", "2", "3", "4", "5", "6"), utf8ToInt))
 ggplot(auto.resid,aes(x=x,y=y,group=col,shape=col)) + geom_line(colour=c("grey70"),size=.75) + geom_point(size=2.5) + ylab("Moran's I") + xlab("Distance (km)") + labs(shape = "Species") + theme_bw() + theme(legend.key = element_blank()) + scale_colour_manual(values=plotPal) + scale_shape_manual(values=shapes) + geom_hline(aes(yintercept=0), linetype=2) + scale_x_continuous(breaks=seq(1, 20, 1))
 
 ggplot(auto.coll,aes(x=x,y=y,group=col,shape=col)) + geom_line(colour=c("grey70"),size=.75) + geom_point(size=2.5) + ylab("Moran's I") + xlab("Distance (km)") + labs(shape = "Species") + theme_bw() + theme(legend.key = element_blank()) + scale_colour_manual(values=plotPal) + scale_shape_manual(values=shapes) + geom_hline(aes(yintercept=0), linetype=2) + scale_x_continuous(breaks=seq(1, 20, 1))
+
+
+#plot marginal effects based on seasons
+load("data/coll_model_data_summer")
+load("output/coll_glm_summer")
+
+occ.summer <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.summer[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.summer[[i]]
+  temp_df <- data.frame(x=data[,occ], y=invcloglog(cbind(1,log(data[,occ]),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,occ])))
+  occ.summer <- rbind(occ.summer,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}  
+
+tiff('figs/occ_summer.tif', pointsize = 8, compression = "lzw", res=300, width = 1100, height = 900)
+ggplot(occ.summer,aes(x=x,y=y,group=col,colour=factor(col))) +
+  geom_line(size=0.3) +
+  ylab("Likelihood of Collision") +
+  xlab("Likelihood of Species Occurrence") +
+  labs(color = "Species") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  scale_colour_manual(values=plotPal) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) +
+  scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
+#guides(colour=FALSE)
+dev.off()
