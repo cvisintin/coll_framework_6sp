7d51172ffb534178b76eba022004d7190de4633f
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-08-16 10:33:13 -0700

    added code to produce seasonal model outputs

--- R/collision_risk_plot.R
+++ R/collision_risk_plot.R
@@ -135,8 +135,14 @@ ggplot(auto.coll,aes(x=x,y=y,group=col,shape=col)) + geom_line(colour=c("grey70"
 
 
 #plot marginal effects based on seasons
-load("data/coll_model_data_summer")
+load("data/coll_model_data_sum")
+load("data/coll_model_data_aut")
+load("data/coll_model_data_win")
+load("data/coll_model_data_spr")
 load("output/coll_glm_summer")
+load("output/coll_glm_autumn")
+load("output/coll_glm_winter")
+load("output/coll_glm_spring")
 
 occ.summer <- NULL
 for (i in 1:nrow(species.table)) {
@@ -148,14 +154,57 @@ for (i in 1:nrow(species.table)) {
   rm(data)
   rm(model)
   rm(temp_df)
-}  
+}
+occ.summer <- cbind(occ.summer,season="summer")
 
-tiff('figs/occ_summer.tif', pointsize = 8, compression = "lzw", res=300, width = 1100, height = 900)
-ggplot(occ.summer,aes(x=x,y=y,group=col,colour=factor(col))) +
-  geom_line(size=0.3) +
+occ.autumn <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.autumn[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.autumn[[i]]
+  temp_df <- data.frame(x=data[,occ], y=invcloglog(cbind(1,log(data[,occ]),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,occ])))
+  occ.autumn <- rbind(occ.autumn,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+occ.autumn <- cbind(occ.autumn,season="autumn")
+
+
+occ.winter <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.winter[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.winter[[i]]
+  temp_df <- data.frame(x=data[,occ], y=invcloglog(cbind(1,log(data[,occ]),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,occ])))
+  occ.winter <- rbind(occ.winter,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+occ.winter <- cbind(occ.winter,season="winter")
+
+occ.spring <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.spring[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.spring[[i]]
+  temp_df <- data.frame(x=data[,occ], y=invcloglog(cbind(1,log(data[,occ]),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,occ])))
+  occ.spring <- rbind(occ.spring,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+occ.spring <- cbind(occ.spring,season="spring")
+
+occ.all <- rbind(occ.summer,occ.autumn,occ.winter,occ.spring)
+
+tiff('figs/occ_seasons.tif', pointsize = 8, compression = "lzw", res=300, width = 1100, height = 900)
+ggplot(occ.all,aes(x=x,y=y,group=interaction(col, season),colour=factor(col),shape=factor(season))) +
+  geom_line(size=0.3,aes(linetype=season)) +
   ylab("Likelihood of Collision") +
   xlab("Likelihood of Species Occurrence") +
-  labs(color = "Species") +
+  labs(colour = "Species", linetype = "Season") +
   theme_bw() +
   theme(legend.key = element_blank()) +
   theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
@@ -165,6 +214,154 @@ ggplot(occ.summer,aes(x=x,y=y,group=col,colour=factor(col))) +
   scale_colour_manual(values=plotPal) +
   theme(text = element_text(size = 8)) +
   scale_x_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) +
+  scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
+#guides(colour=FALSE)
+dev.off()
+
+
+tvol.summer <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.summer[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.summer[[i]]
+  temp_df <- data.frame(x=data[,tvol], y=invcloglog(cbind(1,mean(log(data[,occ])),log(data[,tvol]),(log(data[,tvol]))*(log(data[,tvol])),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tvol])))
+  tvol.summer <- rbind(tvol.summer,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tvol.summer <- cbind(tvol.summer,season="summer")
+
+tvol.autumn <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.autumn[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.autumn[[i]]
+  temp_df <- data.frame(x=data[,tvol], y=invcloglog(cbind(1,mean(log(data[,occ])),log(data[,tvol]),(log(data[,tvol]))*(log(data[,tvol])),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tvol])))
+  tvol.autumn <- rbind(tvol.autumn,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tvol.autumn <- cbind(tvol.autumn,season="autumn")
+
+tvol.winter <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.winter[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.winter[[i]]
+  temp_df <- data.frame(x=data[,tvol], y=invcloglog(cbind(1,mean(log(data[,occ])),log(data[,tvol]),(log(data[,tvol]))*(log(data[,tvol])),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tvol])))
+  tvol.winter <- rbind(tvol.winter,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tvol.winter <- cbind(tvol.winter,season="winter")
+
+tvol.spring <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.spring[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.spring[[i]]
+  temp_df <- data.frame(x=data[,tvol], y=invcloglog(cbind(1,mean(log(data[,occ])),log(data[,tvol]),(log(data[,tvol]))*(log(data[,tvol])),mean(log(data[,tspd]))) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tvol])))
+  tvol.spring <- rbind(tvol.spring,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tvol.spring <- cbind(tvol.spring,season="spring")
+
+tvol.all <- rbind(tvol.summer,tvol.autumn,tvol.winter,tvol.spring)
+
+tiff('figs/tvol_seasons.tif', pointsize = 8, compression = "lzw", res=300, width = 1100, height = 900)
+ggplot(tvol.all,aes(x=x/1000,y=y,group=interaction(col, season),colour=factor(col),shape=factor(season))) +
+  geom_line(size=0.3,aes(linetype=season)) +
+  ylab("Likelihood of Collision") +
+  xlab("Traffic Volume (1000 vehicles/day)") +
+  labs(color = "Species") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  scale_colour_manual(values=plotPal) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(0,25,by=5), expand = c(0, 0), lim=c(0,25)) +
+  scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
+#guides(colour=FALSE)
+dev.off()
+
+
+tspd.summer <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.summer[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.summer[[i]]
+  temp_df <- data.frame(x=data[,tspd], y=invcloglog(cbind(1,mean(log(data[,occ])),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),log(data[,tspd])) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tspd])))
+  tspd.summer <- rbind(tspd.summer,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tspd.summer <- cbind(tspd.summer,season="summer")
+
+tspd.autumn <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.autumn[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.autumn[[i]]
+  temp_df <- data.frame(x=data[,tspd], y=invcloglog(cbind(1,mean(log(data[,occ])),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),log(data[,tspd])) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tspd])))
+  tspd.autumn <- rbind(tspd.autumn,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tspd.autumn <- cbind(tspd.autumn,season="autumn")
+
+tspd.winter <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.winter[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.winter[[i]]
+  temp_df <- data.frame(x=data[,tspd], y=invcloglog(cbind(1,mean(log(data[,occ])),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),log(data[,tspd])) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tspd])))
+  tspd.winter <- rbind(tspd.winter,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tspd.winter <- cbind(tspd.winter,season="winter")
+
+tspd.spring <- NULL
+for (i in 1:nrow(species.table)) {
+  data <- model.data.spring[[i]]
+  colnames(data) <- c("uid","x","y","occ","tvol","tspd","coll")
+  model <- coll.glm.spring[[i]]
+  temp_df <- data.frame(x=data[,tspd], y=invcloglog(cbind(1,mean(log(data[,occ])),mean(log(data[,tvol])),mean((log(data[,tvol]))*(log(data[,tvol]))),log(data[,tspd])) %*% coef(model)), col=rep(toupper(species.table[i,2]), each=length(data[,tspd])))
+  tspd.spring <- rbind(tspd.spring,temp_df)
+  rm(data)
+  rm(model)
+  rm(temp_df)
+}
+tspd.spring <- cbind(tspd.spring,season="spring")
+
+tspd.all <- rbind(tspd.summer,tspd.autumn,tspd.winter,tspd.spring)
+
+tiff('figs/tspd_seasons.tif', pointsize = 8, compression = "lzw", res=300, width = 1100, height = 900)
+ggplot(tspd.all,aes(x=x,y=y,group=interaction(col, season),colour=factor(col),shape=factor(season))) +
+  geom_line(size=0.3,aes(linetype=season)) +
+  ylab("Likelihood of Collision") +
+  xlab("Traffic Speed (km/hour)") +
+  labs(color = "Species") +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
+  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+  scale_colour_manual(values=plotPal) +
+  theme(text = element_text(size = 8)) +
+  scale_x_continuous(breaks=seq(40,110,by=10), expand = c(0, 0), lim=c(40,110)) +
   scale_y_continuous(breaks=seq(0,1,by=.1), expand = c(0, 0), lim=c(0,1)) #+
 #guides(colour=FALSE)
 dev.off()
